efa251851a8f2ab4ad0f9eac12d0a7edd098e38c
UTIII2023 <nocommunistallowed@gmail.com>
2026-03-22 22:44:44 +0300

    22.03.2026 | Progression Checking #1
      - Generated Swedish Study Book A1 -> B1
      - Collected resources for Physics & Modern Physics
      - 4 Year of Computational Astrophysics plan revised
      - Weekly plan revised
      - IELTS plan made
      - New body exercise plan made
      - Finished Boolean Array Indexing in NumPy
      - Continuing Research for studying abroad

--- Year 1/Month 1  🔵 - Scientific Python Toolkit/Practice/boolean_indexing.py
+++ Year 1/Month 1  🔵 - Scientific Python Toolkit/Practice/boolean_indexing.py
@@ -2,7 +2,9 @@ import numpy as np
 
 x = np.array([[1, np.nan], [2, 3], [np.nan, np.nan]])
 y = np.linspace(-5, 5, 5)
-z =
+z = np.arange(25).reshape(5,5)
+t = np.array([[0, 1], [1, 1], [2, 2]])
+e = np.arange(12).reshape(4, 3)
 
 # print(x
 #       ,np.isnan(x)
@@ -17,5 +19,39 @@ z =
 #       ,y<=0
 #       ,y[y<=0]
 #       ,y[y<=0] + 20
-#       ,sep="\n-----\n"),
+#       ,sep="\n-----\n")
 
+new_z = z >= 15
+# print(z
+#     ,new_z
+#     ,new_z[:, 0]
+#     ,z[:, new_z[:, 0]]
+#     ,z[new_z[:, 0], :]
+#     ,z[new_z]
+#     ,sep="\n-----\n"
+# )
+
+rowsum = t.sum(1)
+# """
+# rowsum = array.sum(1)
+# columnsum = array.sum(0)
+# """
+# 
+# print(t
+#       ,rowsum
+#       ,t[rowsum >= 2, :]
+#       ,t[np.ix_(rowsum >= 2)]
+#       ,sep="\n-----\n")
+
+rows_e = (e.sum(1) % 2) == 0
+rows_e_nz = np.nonzero(rows_e)[0]
+columns_e = np.array([0, 2])
+print(e
+      ,np.ndim(rows_e)
+      ,np.ndim(columns_e)
+      ,rows_e
+      ,rows_e_nz
+      ,columns_e
+      ,e[rows_e_nz[:, np.newaxis], columns_e[np.newaxis, :]] # Path 1
+      ,e[np.ix_(rows_e, columns_e)] # Path 2
+      ,sep="\n-----\n")
